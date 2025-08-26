8caa3137976b9bc3e83b2a4e1acf93ebcd973df3
Sithija <sithijahimantha000@gmail.com>
2025-08-26 22:09:16 +0530

    Add environment configuration and update MongoDB connection settings

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import Dotenv from "dotenv";
+
+Dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected successfully 😘");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000 😎");
+app.listen(process.env.port, () => {
+  console.log(`Server is running on port ${process.env.port} 😎`);
 });
