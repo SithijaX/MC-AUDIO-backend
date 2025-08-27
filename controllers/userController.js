b0886efbca9c462f8b71127b4f9aff13ef381d0c
Sithija <sithijahimantha000@gmail.com>
2025-08-27 12:38:42 +0530

    Add user login functionality and JWT authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,12 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const userRegistration = async (req, res) => {
-  const { username, firstName, lastName, email, password, age, profileImage } = req.body;
+  const { username, firstName, lastName, email, password, age, profileImage, role } = req.body;
 
   //check if user already exists
   if (await User.findOne({ email })) {
@@ -15,10 +19,43 @@ export const userRegistration = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, salt);
   //creating user
   try {
-    const newUser = new User({ username, firstName, lastName, email, password: hashedPassword, age, profileImage });
+    const newUser = new User({ username,role, firstName, lastName, email, password: hashedPassword, age, profileImage });
     await newUser.save();
     res.status(201).json({ message: "User created successfully 👤👍", user: newUser });
   } catch (error) {
     res.status(500).json({ message: "Error creating user 👤👎", error: error.message });
   }
 };
+
+
+export async function userLogin(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    //check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found 👤👎" });
+    }
+
+    //check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).json({ message: "wrong password.. try again!" });
+    }
+
+    //create jwt token
+    const token = jwt.sign({
+                email: user.email,
+                password: user.password,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role
+            }, process.env.jwtSecret, {expiresIn: '1h'});
+
+    res.status(200).json({ message: "Login successful 👤👍",
+        user: { name: user.username, email: user.email, role: user.role }, token });
+    } catch (error) {
+        res.status(500).json({ message: "🚫 Login failed!", error: error.message });
+    }
+}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { userRegistration } from "../controllers/userController.js";
+import { userRegistration, userLogin } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", userRegistration);
+userRouter.post("/login", userLogin);
 
 
 
